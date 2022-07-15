457ec016730c63cc1be65c5e9235f3f9146322f6
Diana <dmkirby3@gmail.com>
2022-07-14 23:18:55 -0500

    update styling to navtoggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,10 @@
-alert("Welcome to My Portfolio");
-console.log("Welcome to My Portfolio");
+
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle');
+if (x.className === 'navToggle') {
+  x.className += ' responsive';
+}
+  else {
+    x.className = 'navToggle';
+  }
+}
