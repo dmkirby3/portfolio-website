26d8c26652479bc835b3b503729d05cb77b13526
Diana <dmkirby3@gmail.com>
2022-07-18 20:59:25 -0500

    menutoggle updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
-function menuToggle () {
-  var x = document.getElementById('myNavtoggle')
+function menuToggle (x) {
+  var x = document.getElementById('myNavtoggle');
   if (x.className === 'navToggle') {
-    x.className += ' responsive'
+    x.className += ' responsive';
   } else {
-    x.className = 'navToggle'
+    x.className = 'navToggle';
   }
 }
 
-$('#click_advance').click(function () {
-  $('#display_advance').toggle('1000')
-  $('i', this).toggleClass('fas fa-bars fas fa-x')
-})
+$('#click_advance').click(function() {
+  $('#display_advance').toggle('1000');
+  $("i", this).toggleClass("fas fa-bars fas fa-x");
+});
